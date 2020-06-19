b676ff65eca344d9cb5d2535206e211dbc75a270
Luke McCann <developer-lukemccann@outlook.com>
2020-06-19 15:13:45 +0100

    Add Query_type model

--- src/app/Models/Campaign.php
+++ src/app/Models/Campaign.php
@@ -11,10 +11,19 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Campaign extends Model
 {
+        private String $id;
         private String $name;
         private String $desc;
 
         // Getters and Setters
+        public function getId() {
+            return $id;
+        }
+
+        public function setId(String $id) {
+            $this->id = $id;
+        }
+
         public function getName() {
             return $name;
         }
