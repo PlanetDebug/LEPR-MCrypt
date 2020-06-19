829077246e9adfa04438a980246730c6ac92985e
Luke McCann <developer-lukemccann@outlook.com>
2020-06-19 14:13:21 +0100

    Bugfix: fixes issue with SQL syntax error on docker-compose up

--- init/init.sql
+++ init/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE app_db;
+USE app_db;
 
 /* Create tables with foreign keys first*/
 CREATE TABLE query_type (
@@ -25,73 +26,73 @@ CREATE TABLE person (
     last_name VARCHAR(50) NOT NULL,
     phone VARCHAR(13),
     email VARCHAR(65),
-    query_id SERIAL REFERENCES query_type(id) UNIQUE (query_id),
-    call_stats_id SERIAL REFERENCES call_stats(id) UNIQUE (call_stats_id),
-    campaign_id SERIAL REFERENCES campaign(id) UNIQUE (campaign_id)
+    query_id SERIAL REFERENCES query_type(id),
+    call_stats_id SERIAL REFERENCES call_stats(id),
+    campaign_id SERIAL REFERENCES campaign(id)
 );
 
 /* Populate query_type table */
 INSERT INTO query_type (id, title)
 VALUES (
-    "1234",
-    "SALE MADE"
+    '1234',
+    'SALE MADE'
 );
 
 INSERT INTO query_type(id, title) 
 VALUES(
-    "5678",
-    "DECLINED OFFER"
+    '5678',
+    'DECLINED OFFER'
 );
 
 /* Populate call_stats table */
 INSERT INTO call_stats(id, call_length, recording_url)
 VALUES(
-    "5678",
-    "01:56:34",
-    "http://remote.system/recording/5678"
+    '5678',
+    '01:56:34',
+    'http://remote.system/recording/5678'
 );
 
 INSERT INTO call_stats(id, call_length, recording_url)
 VALUES(
-    "1213",
-    "00:56:43",
-    "http://remote.system/recording/1213"
+    '1213',
+    '00:56:43',
+    'http://remote.system/recording/1213'
 );
 
 /* Populate campaign table */
 INSERT INTO campaign (id, campaign_name, campaign_desc)
 VALUES (
-    "1011",
-    "Campaign A",
-    "A lovely campaign for Michael"
+    '1011',
+    'Campaign A',
+    'A lovely campaign for Michael'
 );
 
 INSERT INTO campaign (id, campaign_name, campaign_desc)
 VALUES (
-    "1516",
-    "Campaign B",
-    "A different campaign not for Michael"
+    '1516',
+    'Campaign B',
+    'A different campaign not for Michael'
 );
 
 /* Populate person table */
 INSERT INTO person (first_name, last_name, phone, email, query_id, call_stats_id, campaign_id)
 VALUES (
-    "Michael",
-    "Collier",
-    "07707000000",
-    "bigmike@collier.com",
-    "1234",
-    "5678",
-    "1011"
+    'Michael',
+    'Collier',
+    '07707000000',
+    'bigmike@collier.com',
+    '1234',
+    '5678',
+    '1011'
 );
 
 INSERT INTO person (first_name, last_name, phone, email, query_id, call_stats_id, campaign_id)
 VALUES (
-    "Jimmy",
-    "Joe",
-    "07707000001",
-    "bigjim@collier.com",
-    "5678",
-    "1213",
-    "1516"
+    'Jimmy',
+    'Joe',
+    '07707000001',
+    'bigjim@collier.com',
+    '5678',
+    '1213',
+    '1516'
 );
