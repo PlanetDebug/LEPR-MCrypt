a358d37cff28096d28d0e565c1f57cc86d422ac4
Luke McCann <developer-lukemccann@outlook.com>
2020-06-19 15:04:56 +0100

    Add Person model

---
+++ src/app/Models/Person.php
@@ -0,0 +1,56 @@
+<?php 
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Person
+ * 
+ * Model class for attributing person data.
+ */
+class Person extends Model
+{
+    private String $first_name;
+    private String $last_name;
+    private String $phone_num;
+    private String $email;
+
+    // Getters and Setters
+    public function getFirstName() {
+        return $first_name;
+    }
+
+    public function setFirstName(String $name) {
+        $first_name = $name;
+    }
+
+    public function getLastName() {
+        return $last_name;
+    }
+
+    public function setLastName(String $name) {
+        $last_name =  $name;
+    }
+
+    public function getPhoneNum() {
+        return $phone_num;
+    }
+
+    public function setPhoneNum(String $num) {
+        $phone_num = $num;
+    }
+
+    public function getEmail() {
+        return $email;
+    }
+
+    public function toString() {
+        return "name: " . $first_name . $last_name . "<br />" . 
+                "phone: " . $phone_num . "<br />" .
+                "email: " . $email 
+        ; 
+    }
+}
+
+?>
